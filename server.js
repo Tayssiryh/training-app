bd2015dbaef9cacf1c60fa9debd3b936cb0dc100
tayssir yahyaoui <tayssir.yahyaoui@etudiant-enit.utm.tn>
2024-03-21 14:16:18 +0100

    nodejs

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello team Harington');
+  res.send('Hello team Harington!');
 });
 
 app.listen(PORT, HOST, () => {
